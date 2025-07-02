d4fa79802c52e6121844bc89b28664eb0295084a
Narayan <narayanthakur7348@gmail.com>
2025-07-02 10:11:32 +0530

    Added program for Company_(Company and Departments(Composition))-Object_Oriented_Design_Principles

---
+++ Object_Oriented_Design_Principles/Company_.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+class Employee {
+    String name;
+
+    Employee(String name) {
+        this.name = name;
+    }
+}
+
+class Department {
+    String deptName;
+    ArrayList<Employee> employees = new ArrayList<>();
+
+    Department(String deptName) {
+        this.deptName = deptName;
+    }
+
+    void addEmployee(Employee emp) {
+        employees.add(emp);
+    }
+
+    void showEmployees() {
+        System.out.println("Department: " + deptName);
+        for (Employee e : employees) {
+            System.out.println(" - " + e.name);
+        }
+    }
+}
+
+class Company {
+    ArrayList<Department> departments = new ArrayList<>();
+
+    void addDepartment(Department dept) {
+        departments.add(dept);
+    }
+
+    void showCompanyStructure() {
+        for (Department d : departments) {
+            d.showEmployees();
+        }
+    }
+}
+
+public class Company_ {
+    public static void main(String[] args) {
+        Company company = new Company();
+        Department d1 = new Department("HR");
+        Department d2 = new Department("IT");
+
+        d1.addEmployee(new Employee("Alice"));
+        d2.addEmployee(new Employee("Bob"));
+
+        company.addDepartment(d1);
+        company.addDepartment(d2);
+
+        company.showCompanyStructure();
+    }
+}
