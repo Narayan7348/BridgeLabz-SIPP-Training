4a723b26fc2389760b7cd6574fd0ba5e2ed17cd3
Narayan <narayanthakur7348@gmail.com>
2025-07-02 10:19:45 +0530

    Added program for Hospital_Patient(Hospital,Doctors and Patients(Association and Communication))-Object_Oriented_Design_Principles

---
+++ Object_Oriented_Design_Principles/Hospital_Patient.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Patient {
+    String name;
+
+    Patient(String name) {
+        this.name = name;
+    }
+}
+
+class Doctor {
+    String name;
+
+    Doctor(String name) {
+        this.name = name;
+    }
+
+    void consult(Patient p) {
+        System.out.println(name + " is consulting " + p.name);
+    }
+}
+
+class Hospital {
+    ArrayList<Doctor> doctors = new ArrayList<>();
+    ArrayList<Patient> patients = new ArrayList<>();
+
+    void addDoctor(Doctor d) {
+        doctors.add(d);
+    }
+
+    void addPatient(Patient p) {
+        patients.add(p);
+    }
+}
+
+public class Hospital_Patient {
+    public static void main(String[] args) {
+        Hospital hospital = new Hospital();
+        Doctor d1 = new Doctor("Dr. Ray");
+        Patient p1 = new Patient("Tom");
+
+        hospital.addDoctor(d1);
+        hospital.addPatient(p1);
+
+        d1.consult(p1);
+    }
+}
+
