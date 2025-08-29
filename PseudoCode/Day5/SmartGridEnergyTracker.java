f2d672050c3924056c636129f94f58f30b7304c8
Narayan <narayanthakur7348@gmail.com>
2025-08-29 10:39:44 +0530

    commit

--- PseudoCode/Day5/SmartGridEnergyTracker.java
+++
@@ -1,9 +0,0 @@
-/*Map<String, Map<String, List<Integer>>> data; // Zone → Day → Readings
-
-// Process
-for each zone in data:
-    for each day in zone:
-        List<Integer> readings = data[zone][day];
-        int peak = max(readings);
-        print("Zone: " + zone + ", Day: " + day + ", Peak: " + peak);
-*/
